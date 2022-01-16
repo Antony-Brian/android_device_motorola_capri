a26a838eea425fba2951fb158aa0a855c60e795b
Antony-Brian <78004742+Antony-Brian@users.noreply.github.com>
2022-01-16 17:58:09 -0300

    Update omni_capri.mk

--- omni_capri.mk
+++ omni_capri.mk
@@ -14,12 +14,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/motorola/capri/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := capri
-PRODUCT_NAME := omni_capri
+PRODUCT_NAME := twrp_capri
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g(10)
 PRODUCT_MANUFACTURER := motorola
